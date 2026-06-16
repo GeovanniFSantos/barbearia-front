86a31a5d301ee469bd63628f596f7c3474ea59d0
Geovanni <geovannysantos@outlook.com>
2026-06-16 12:31:02 -0300

    Modificando a tela de Super admin

--- public/js/dashboard_superAdmin.js
+++ public/js/dashboard_superAdmin.js
@@ -1,5 +1,24 @@
-        // Função para Abrir/Fechar o Modal
         function toggleModal() {
             const modal = document.getElementById('modalCadastro');
-            modal.classList.toggle('hidden');
+            const modalContent = document.getElementById('modalContent');
+            
+            console.log("Comando recebido: Acionando o Modal!");
+
+            if (modal.classList.contains('hidden')) {
+                // Remove a trava de visibilidade
+                modal.classList.remove('hidden');
+                // Pequeno delay para permitir que o Tailwind processe a animação
+                setTimeout(() => {
+                    modalContent.classList.remove('scale-95', 'opacity-0');
+                    modalContent.classList.add('scale-100', 'opacity-100');
+                }, 10);
+            } else {
+                // Inicia a animação de encolhimento
+                modalContent.classList.remove('scale-100', 'opacity-100');
+                modalContent.classList.add('scale-95', 'opacity-0');
+                // Espera a animação terminar (300ms) para esconder a div inteira
+                setTimeout(() => {
+                    modal.classList.add('hidden');
+                }, 300);
+            }
         }
